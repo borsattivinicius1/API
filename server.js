4d289e35a7c26e959db792fd78c1f0ed0cc78f1f
borsattivinicius1 <borsattivinicius1@gmail.com>
2026-02-20 14:01:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-import express from 'express'
+import express, { json } from 'express'
 
 
 const app = express()
+app.use(express.json())
+
+
+const users=[]
+
+app.post('/usuarios', (req,res) =>{
+
+        users.push(req.body)
+        res.status(201).json(req.body)
+})
+
 
 
 app.get('/usuarios' , (req,res) =>{
-
-        res.send('Ok , deu bom')
-
+        res.status(200).json(users)
 })
 
 app.listen(3332)
+
+
+
+
+// borsattivinicius1 , Senha: v1s9hqLyQ91J4UnV
